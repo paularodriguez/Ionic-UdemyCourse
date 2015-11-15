6df044f78e69cea5f19b2b6a4d6d0a70d8e07d55
Paula Rodriguez <paula.rp93@gmail.com>
2015-11-15 21:01:20 +0100

    customizing resources

---
+++ ionized-notes/www/js/app.js
@@ -0,0 +1,84 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+(function(){
+var app = angular.module('mynotes', ['ionic', 'mynotes.notestore']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider.state('list',{
+    url: '/list',
+    templateUrl: 'templates/list.html'
+  });
+
+  $stateProvider.state('add',{
+    url: '/add',
+    templateUrl: 'templates/edit.html',
+    controller: 'AddCtrl'
+  });
+
+  $stateProvider.state('edit',{
+    url: '/edit/:noteId',
+    templateUrl: 'templates/edit.html',
+    controller: 'EditCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/list');
+});
+
+app.controller('ListCtrl', function($scope, NoteStore){
+  
+  $scope.reordering = false;
+  $scope.notes = NoteStore.list();
+
+  $scope.remove = function(noteId){
+    NoteStore.remove(noteId);
+  };
+
+  $scope.move = function(note, fromIndex, toIndex){
+    NoteStore.move(note, fromIndex, toIndex);
+  };
+
+  $scope.toggleReordering = function(){
+    $scope.reordering = !$scope.reordering;
+  }
+});
+
+app.controller('AddCtrl', function($scope, $state, NoteStore){  
+
+  $scope.note = {
+    id: new Date().getTime().toString(),
+    title: '',
+    description: ''
+  };
+
+  $scope.save = function(){
+    NoteStore.create($scope.note);
+    $state.go('list'); //we return to the main page
+  };
+});
+
+app.controller('EditCtrl', function($scope, $state, NoteStore){  /*$state service allow us to get de param in url*/
+
+  $scope.note = angular.copy(NoteStore.get($state.params.noteId)); //provides a note copy in order to get the original copy if user don't save the changes
+
+  $scope.save = function(){
+    NoteStore.update($scope.note);
+    $state.go('list'); //we return to the main page
+  };
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+}());
---
+++ ionized-notes/www/js/notestore.js
@@ -0,0 +1,59 @@
+angular.module('mynotes.notestore',[])
+
+.factory('NoteStore', function(){
+
+  var notes = angular.fromJson(window.localStorage['notes'] || '[]');
+
+  function persist(){
+    window.localStorage['notes'] = angular.toJson(notes);
+  }
+
+  return{
+
+    list: function(){
+      return notes;
+    },
+
+    get: function(noteId){
+       for (var i = 0; i < notes.length; i++){
+          if(notes[i].id === noteId){
+            return notes[i];
+          }
+        }
+        return undefined;
+    },
+
+    create: function(note){
+        notes.push(note);
+        persist();
+    },
+
+    update: function(note){
+      for (var i = 0; i < notes.length; i++){
+        if(notes[i].id === note.id){
+          notes[i] = note;  //if the notes are the same, this note is replaced
+          persist();
+          return;
+        }
+      }
+    },
+
+    move: function(note, fromIndex, toIndex){
+      notes.splice(fromIndex,1);
+      notes.splice(toIndex,0, note);
+      persist();
+    },
+
+    remove: function(noteId){
+       for (var i = 0; i < notes.length; i++){
+        if(notes[i].id === noteId){
+          notes.splice(i,1);
+          persist();
+          return;     
+        }
+      }
+    }
+
+  };
+
+});
